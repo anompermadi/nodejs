43e31e05d6d05392902881943a36d7587016614c
anompermadi <anom.permadi@outlook.com>
2022-05-11 15:51:31 +0700

    buffer encoding

---
+++ buffer-encoding.mjs
@@ -0,0 +1,8 @@
+const buffer = Buffer.from("Arsenius Anom Permadi", "utf8");
+
+console.info(buffer.toString());
+console.info(buffer.toString("hex"));
+console.info(buffer.toString("base64"));
+
+const bufferBase64 = Buffer.from("QXJzZW5pdXMgQW5vbSBQZXJtYWRp", "base64");
+console.info(bufferBase64.toString("utf8"));
