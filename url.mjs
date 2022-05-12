0f36b18962341d8136ec550ab028659f4c19b0ba
anompermadi <anom.permadi@outlook.com>
2022-05-12 09:25:12 +0700

    url

--- url.mjs
+++ url.mjs
@@ -2,6 +2,9 @@ import {URL} from "url";
 
 const anom = new URL("https://broanom.com/learn?class=nodejs");
 
+anom.host = "www.anompermadi.com";
+anom.searchParams.append("status", "premium");
+
 console.info(anom.toString());
 console.info(anom.href);
 console.info(anom.protocol);
