f5b050719e5a6746aa9c0fe99fa10ead424c4561
anompermadi <anom.permadi@outlook.com>
2022-05-17 13:41:31 +0700

    update library

--- node-npm/package.js
+++ node-npm/package.js
@@ -1,5 +1,9 @@
+import { min, max } from "node-npm-library/number";
 import { sayHello, sum } from "node-npm-library";
 
 console.info(sayHello("anom"));
 
 console.info(sum([10, 10, 10, 10, 10]));
+
+console.info(min(10, 20));
+console.info(max(10, 20));
